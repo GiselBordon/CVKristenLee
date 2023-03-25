b8de77c398da8a7849b43223e729d3af05305ea8
Gisel Bordón <giisebordon@gmail.com>
2023-03-25 15:09:56 -0300

    Adición de comentario en JS

--- js/function.js
+++ js/function.js
@@ -1,3 +1,5 @@
+/**Botón para cambiar texto antes del pie de página */
+
 document.getElementById('boton').onclick = function () {
     console.log("Capturamos el evento click");
     document.getElementById("ultimoparrafo").innerHTML = "¡Saludos!"; 
