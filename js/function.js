0ef50b01a73ad65f0cb6c5b868738c13f74b0dfb
Gisel Bordón <giisebordon@gmail.com>
2023-03-26 19:14:19 -0300

    Adición de sombra de título1 y cambio de color de titulo2

--- js/function.js
+++ js/function.js
@@ -4,3 +4,11 @@ document.getElementById('boton').onclick = function () {
     console.log("Capturamos el evento click");
     document.getElementById("ultimoparrafo").innerHTML = "¡Saludos!"; 
 }
+
+/**Título 2 cambia de color cuando mouse se mueve por encima */
+
+const test = document.getElementById("titulo2");
+test.addEventListener("mouseover", function (event) {
+    event.target.style.color = "#ab1b68";
+}, false); 
+
